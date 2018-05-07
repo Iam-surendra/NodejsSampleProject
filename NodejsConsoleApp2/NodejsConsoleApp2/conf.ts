979caf957dc364733408a6ae241429e819178aab
v-suthot <v-suthot@MININT-MQUR1J8>
2018-05-07 16:30:40 +0530

    Test Check In First

---
+++ NodejsConsoleApp2/NodejsConsoleApp2/conf.ts
@@ -0,0 +1,12 @@
+﻿import { Config } from 'protractor';
+
+export let config: Config = {
+    directConnect: true,
+    framework: 'jasmine',
+    capabilities: {
+        browserName: 'chrome'
+    },
+    specs: ['spec.js'],
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    noGlobals: true
+};
